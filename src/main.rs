2dbfba9173d8ed69f40c9715875ff7383f66fd4b
Ed Fawcett-Taylor <edwardpaultaylor@gmail.com>
2022-11-19 22:09:58 +0000

    add extra `vec!` to input of `Polygon::new`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use auto_gis_with_rust::{error::GeometryError, polygon::Polygon};
 
 fn main() -> Result<(), GeometryError> {
-    let polygon = Polygon::new(vec![[0., 0.], [0., 1.], [1., 1.]])?;
+    let polygon = Polygon::new(vec![vec![[0., 0.], [0., 1.], [1., 1.]]])?;
     dbg!(polygon);
     Ok(())
 }
